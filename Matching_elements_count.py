ee2660319e2dda07adefd797169b89dc33e7b805
XILOUYI <40003769+XILOUYI@users.noreply.github.com>
2020-09-03 22:42:39 -0500

    Update Matching_elements_count.py

--- Matching_elements_count.py
+++ Matching_elements_count.py
@@ -11,5 +11,10 @@ list2 = list(df2.carat)
 list3 = list(df3.carat)
 #p = set(list2)&set(list1)&set(list3)
 p = set(list1).intersection(set(list2))
+#Python Set union() Method，Return a set that contains all items from both sets, duplicates are excluded
+b = set(list1).union(set(list2))
+# Python code to get the difference between two sets， using difference() between set A and set B 
+s = set(list1).difference(set(list2))
+x.union(y)
 print(p)
 print(len(p))
